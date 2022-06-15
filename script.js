1faaaa7ee79686faef8d82a316c37a16ab08d87c
Gabriel Leite <gabriel.leite.leandro@gmail.com>
2022-06-15 20:22:05 -0300

    projeto 3 final corrigido

--- script.js
+++ script.js
@@ -1,36 +1,133 @@
-function selecionar(elemento) {
+let tituloPrato;
+let tituloBebida;
+let tituloSobremesa;
 
-    let nomeComida = elemento.parentNode.className
-    console.log(nomeComida)
+//Criando 3 variaveis do tipo let
+let precoBebida, precoPrato, precoSobremesa, total, nome, endereco;
 
 
-   const escolhido = document.querySelectorAll(`.${nomeComida} .selecionado`);
+function converterNumero (precoString) {
 
-   console.log(escolhido)
-   if (escolhido.length > 0) {
-    escolhido[0].classList.remove("selecionado");
+    let preco = precoString.replace("R$ ","");
+    preco = preco.replace(",", ".");
+    preco = Number(preco) * 100;
 
- }
+    return preco;
+}
 
-    elemento.classList.toggle("selecionado");
+function selecionarPrato (elemento) {
+    const cardClicado = document.querySelector('.pratos .selecionado');
+
+    if (cardClicado !== null) {
+        cardClicado.classList.remove('selecionado');
+    }
+    elemento.classList.add('selecionado');
+    tituloPrato = elemento.querySelector('.titulo').innerHTML;
+   
+    precoPrato = elemento.querySelector('.preco').innerHTML;
+    precoPrato = converterNumero(precoPrato);
+
+    fazerPedido();
+
+}
+
+function selecionarBebida (elemento) {
+    const cardClicado = document.querySelector('.bebidas .selecionado');
+
+    if (cardClicado !== null) {
+        cardClicado.classList.remove('selecionado');
+    }
+    
+    elemento.classList.add('selecionado');
+    
+    tituloBebida = elemento.querySelector('.titulo').innerHTML;
+    precoBebida = elemento.querySelector('.preco').innerHTML;
+    precoBebida = converterNumero(precoBebida);
+    fazerPedido();
+}
+
+function selecionarSobremesa (elemento) {
+    const cardClicado = document.querySelector('.sobremesas .selecionado');
+
+    if (cardClicado !== null) {
+        cardClicado.classList.remove('selecionado');
+    }
+    
+    elemento.classList.add('selecionado');
+    tituloSobremesa = elemento.querySelector('.titulo').innerHTML;
+    precoSobremesa = elemento.querySelector('.preco').innerHTML;
+    precoSobremesa = converterNumero(precoSobremesa);
+    fazerPedido();
+    
+}
+
+function fazerPedido () {
+
+    //TEM UMA FORMA MELHOR DE FAZER &&
+    if (tituloPrato !== undefined) {
+        if (tituloBebida !== undefined) {
+            if (tituloSobremesa !== undefined) {
+                console.log('VOCE PODE FAZER O PEDIDO')
+                const botaoFecharPedido = document.querySelector('.fazer-pedido');
+
+                botaoFecharPedido.classList.add('ativo');
+                botaoFecharPedido.innerHTML = 'Fazer o pedido'
+            }
+        }
+    } 
+}
+
+
+function mostrarConfirmarPedido () {
 
     
-   fecharPedido();
+    if (tituloPrato !== undefined && tituloBebida !== undefined && tituloSobremesa !== undefined){
+
+
+        nome = prompt('Qual o seu nome?');
+        endereco = prompt('Onde devo entregar?');
+
+        const overlay = document.querySelector('.overlay');
+
+
+        overlay.querySelector('.prato .nome').innerHTML = tituloPrato;
+        overlay.querySelector('.prato .preco').innerHTML = (precoPrato / 100).toFixed(2);
+
+        overlay.querySelector('.bebida .nome').innerHTML = tituloBebida;
+        overlay.querySelector('.bebida .preco').innerHTML = (precoBebida / 100).toFixed(2);
+
+
+        overlay.querySelector('.sobremesa .nome').innerHTML = tituloSobremesa;
+        overlay.querySelector('.sobremesa .preco').innerHTML = (precoSobremesa / 100).toFixed(2);
+
+        total = precoPrato + precoBebida + precoSobremesa;
+        overlay.querySelector('.total .preco-total').innerHTML = `R$ ${(total / 100).toFixed(2)}`;
+
+        overlay.classList.remove('escondido');
+    }
+ 
 }
-function fecharPedido() {
-   let selecionados = document.querySelectorAll(".selecionado");
 
-   const botaoFechar = document.querySelector(".botao-barra h1")
-   if (selecionados.length === 3) {
+function cancelarOverlay() {
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.add('escondido');
+}
 
-      botaoFechar.innerHTML = "Fechar pedido";
+function enviarPedido () {
 
-   }
-   const botaoVerde = document.querySelector(".botao-barra")
-   if (selecionados.length === 3) {
+    let msg = `Olá, gostaria de fazer o pedido:
+    - Prato: ${tituloPrato}
+    - Bebida: ${tituloBebida}
+    - Sobremesa: ${tituloSobremesa}
+    Total: R$ ${(total / 100).toFixed(2)}
+    
+    Nome: ${nome}
+    Endereço: ${endereco}
+    `
 
-      botaoVerde.classList.add("botao-verde");
+    msg = encodeURIComponent(msg);
 
-   }
-
+    const numeroAtendimento = '329999999999'
+    const linkZapvac = `https://wa.me/${numeroAtendimento}?text=${msg}`
+    window.open(linkZapvac);
 }
